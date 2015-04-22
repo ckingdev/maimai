c8d3fe202259cb8d3537af42650d3eb3af14cf5a
M. Cameron Palone <mcameronpalone@gmail.com>
2015-04-21 19:32:28 -0600

    Update main script.

--- main/main.go
+++ main/main.go
@@ -3,13 +3,33 @@ package main
 import (
 	"runtime"
 	"time"
+	"flag"
 
 	"github.com/apologue-dot-net/maimai"
 )
 
+var roomName string
+var nick string
+var logPath string
+var dbPath string
+
+func init() {
+	const (
+		defaultRoom = "test"
+		defaultNick = "MaiMai"
+		defaultLog = "room_test.log"
+		defaultDB = "room_test.db"
+	)
+	flag.StringVar(&roomName, "room", defaultRoom, "room for the bot to join")
+	flag.StringVar(&nick, "nick", defaultNick, "nick for the bot to use")
+	flag.StringVar(&logPath, "log", defaultLog, "path for the bot's log")
+	flag.StringVar(&dbPath, "db", defaultDB, "path for the bot's db")
+}
+
 func main() {
+	flag.Parse()
 	runtime.GOMAXPROCS(runtime.NumCPU() - 1)
-	roomCfg := &maimai.RoomConfig{"MaiMai", "", "testroom.db", "errors.log", []maimai.Handler{}}
+	roomCfg := &maimai.RoomConfig{"MaiMai", "", "testroom.db", "errors.log"}
 	connCfg := &maimai.ConnConfig{"test", 5, time.Duration(1) * time.Second}
 
 	conn, err := maimai.NewConn(connCfg)
@@ -21,10 +41,6 @@ func main() {
 		panic(err)
 	}
 	err = room.SendNick("MaiMai")
-	b, err := maimai.NewBot(room, botCfg)
-	if err != nil {
-		panic(err)
-	}
-	b.Room.SendNick(roomCfg.Nick)
-	b.Run()
+	room.SendNick(roomCfg.Nick)
+	room.Run()
 }
