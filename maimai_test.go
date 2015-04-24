e0d8cdd380d4ea8a29561470bc15ef2ba419bf3e
M. Cameron Palone <mcameronpalone@gmail.com>
2015-04-24 14:00:36 -0600

    Changed test harness to store pointer to testing.T object.

--- maimai_test.go
+++ maimai_test.go
@@ -1,7 +1,7 @@
 package maimai
 
 import (
-	"fmt"
+	"encoding/json"
 	"testing"
 	"time"
 )
@@ -9,15 +9,16 @@ import (
 type TestHarness struct {
 	outbound *(chan interface{})
 	inbound  *(chan *PacketEvent)
+	t        *testing.T
 }
 
-func NewTestHarness() (*Room, *TestHarness) {
+func NewTestHarness(t *testing.T) (*Room, *TestHarness) {
 	roomCfg := &RoomConfig{"MaiMai", "", "test.db", "test.log"}
 	room, err := NewRoom(roomCfg, "test")
 	if err != nil {
 		panic(err)
 	}
-	th := &TestHarness{&room.outbound, &room.inbound}
+	th := &TestHarness{&room.outbound, &room.inbound, t}
 	return room, th
 }
 
@@ -25,21 +26,21 @@ func (th *TestHarness) AssertReceivedSendText(text string) {
 	msg := <-*th.outbound
 	packet, ok := msg.(PacketEvent)
 	if !ok {
-		panic("Could not assert message as PacketEvent.")
+		th.t.Fatal("Could not assert message as PacketEvent.")
 	}
 	if packet.Type != SendType {
-		panic("Packet is not of type 'send'. Got " + string(packet.Type))
+		th.t.Fatalf("Packet is not of type 'send'. Got %s", packet.Type)
 	}
 	payload, err := packet.Payload()
 	if err != nil {
-		panic("Could not extract packet payload. Error: " + err.Error())
+		th.t.Fatalf("Could not extract packet payload. Error: %s", err)
 	}
 	data, ok := payload.(*SendCommand)
 	if !ok {
-		panic("Could not assert payload as *SendCommand.")
+		th.t.Fatal("Could not assert payload as *SendCommand.")
 	}
 	if data.Content != text {
-		panic(fmt.Sprintf("Message content does not match text. Expected '%s', got '%s'", text, data.Content))
+		th.t.Fatalf("Message content does not match text. Expected '%s', got '%s'", text, data.Content)
 	}
 }
 
@@ -47,33 +48,63 @@ func (th *TestHarness) AssertReceivedNick() {
 	msg := <-*th.outbound
 	packet, ok := msg.(*PacketEvent)
 	if !ok {
-		panic("Could not assert message as *PacketEvent.")
+		th.t.Fatal("Could not assert message as *PacketEvent.")
 	}
 	if packet.Type != "nick" {
-		panic("Packet is not of type 'nick'.")
+		th.t.Fatal("Packet is not of type 'nick'.")
 	}
 }
 
+func (th *TestHarness) SendSendEvent(text string, parent string, sender string) {
+	payload, _ := json.Marshal(Message{
+		Content: text,
+		Parent:  parent,
+		Sender:  User{Name: sender}})
+	msg := PacketEvent{
+		Type: SendEventType,
+		Data: payload}
+	*th.inbound <- &msg
+}
+
 func TestConnect(t *testing.T) {
-	room, _ := NewTestHarness()
+	room, _ := NewTestHarness(t)
 	defer room.db.Close()
 	if err := room.sr.Connect("test"); err != nil {
 		t.Fatal("Could not connect to mock interface.")
 	}
+	time.Sleep(time.Second)
 }
 
 func TestRun(t *testing.T) {
-	room, _ := NewTestHarness()
+	room, _ := NewTestHarness(t)
 	defer room.db.Close()
 	go room.Run()
 	time.Sleep(time.Second * time.Duration(3))
 }
 
 func TestSendText(t *testing.T) {
-	room, th := NewTestHarness()
+	room, th := NewTestHarness(t)
 	defer room.db.Close()
 	go room.Run()
 	room.SendText("test text", "")
-	// <-*th.outbound
 	th.AssertReceivedSendText("test text")
+	time.Sleep(time.Second)
+}
+
+func TestPingCommand(t *testing.T) {
+	room, th := NewTestHarness(t)
+	defer room.db.Close()
+	go room.Run()
+	th.SendSendEvent("!ping", "", "test")
+	th.AssertReceivedSendText("pong!")
+	time.Sleep(time.Second)
+}
+
+func TestScritchCommand(t *testing.T) {
+	room, th := NewTestHarness(t)
+	defer room.db.Close()
+	go room.Run()
+	th.SendSendEvent("!scritch", "", "test")
+	th.AssertReceivedSendText("/me bruxes")
+	time.Sleep(time.Second)
 }
